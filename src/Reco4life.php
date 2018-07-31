40ccdc06d262ec74b374a473ba7cdaafd12ce17a
帅纯亮 <shuaichunliang@hooook.com>
2018-07-31 03:10:36 +0000

    init project

---
+++ src/Reco4life.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: purelightme
+ * Date: 2018/8/2
+ * Time: 15:32
+ */
+
+namespace Purelightme;
+
+class Reco4life
+{
+    public $userName = '';
+    public $apiKey = '';
+    public $token = '';
+    public $urlPrefix = 'http://api.reco4life.com/v/api.1.1/';
+
+    public function construct($userName, $apiKey)
+    {
+        $this->userName = $userName;
+        $this->apiKey = $apiKey;
+    }
+
+    public function sendGetRequest($action,$params,$token = '')
+    {
+        $params = http_build_query($params);
+        $ch = curl_init($this->urlPrefix . $action .'?'. $params);
+        if ($token){
+            $headers = [
+                'token' => $token
+            ];
+            curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+        }
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        $res = curl_exec($ch);
+        if (curl_errno($ch)){
+            return [
+                'result' => curl_error($ch)
+            ];
+        }
+        return json_decode($res,true);
+    }
+
+    public function getToken()
+    {
+        $action = 'get_token';
+        $ret =  $this->sendGetRequest($action, [
+            'user_name' => $this->userName,
+            'api_key' => $this->apiKey
+        ]);
+        if (isset($ret['token'])){
+            $this->token = $ret['token'];
+        }
+        return $ret;
+    }
+
+    public function itemList($params)
+    {
+        if (!$this->token)
+            $this->getToken();
+        $action = 'item_list';
+        return $this->sendGetRequest($action, $params);
+    }
+
+    public function itemSwitch($params)
+    {
+        if (!$this->token)
+            $this->getToken();
+        $action = 'item_switch';
+        return $this->sendGetRequest($action, $params);
+    }
+}
